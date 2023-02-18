fca410ed2d5dd8dbecf57de48cf2670a0ec3551e
57qy654 <Jude.Pitschka@gmail.com>
2023-02-17 18:30:58 -0600

    Transferred Assets
    Moved assets from old project to new project.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Camera camera;
+    private  Rigidbody2D rigidbody;
+
+    private Vector2 velocity;
+    private float inputAxis;
+
+    public float moveSpeed = 8f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        HorizontalMovement();
+    }
+
+
+    private void HorizontalMovement()
+    {
+        inputAxis = Input.GetAxis("Horizontal");
+        velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed, moveSpeed * Time.deltaTime);
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody.position;
+        position += velocity * Time.fixedDeltaTime;
+
+        Vector2 leftEdge = camera.ScreenToWorldPoint(Vector2.zero);
+        Vector2 rightEdge = camera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        position.x = Mathf.Clamp(position.x, leftEdge.x + 0.5f, rightEdge.x - 0.5f);
+
+        rigidbody.MovePosition(position);
+    }
+}
