ea8d7718a7481869629b50ecce098deee3697e33
ImSoBogus <willybogus11@gmail.com>
2023-02-19 17:58:10 -0600

    Game Manager Level Reset
    added code to the game manager that resets based off things like lives, wins, losses etc.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; } // allows so that
+
+    public int world { get; private set; } // public get, private set
+    public int stage { get; private set; } 
+    public int lives { get; private set; }
+
 
     private void Awake()
     {
@@ -29,4 +35,53 @@ public class GameManager : MonoBehaviour
         
     }
 
+    private void Start()
+    {
+        NewGame(); // creates new game when its started
+    }
+
+    private void NewGame()
+    {
+        lives = 3;
+
+        LoadLevel(1, 1); //creates a new game with given level and lives
+    }
+
+    private void LoadLevel(int world, int stage)
+    {
+        this.world = world;
+        this.stage = stage;
+
+        SceneManager.LoadScene($"{world}-{stage}"); // loads the given stage level
+    }
+
+    public void NextLevel()
+    {
+        LoadLevel(world, stage + 1);
+    }
+
+    public void ResetLevel(float delay)
+    {
+        Invoke(nameof(ResetLevel), delay);
+        // makes it so game doesnt restart immediatly
+    }
+
+    public void ResetLevel()
+    {
+        lives -= 1; // decreases amount of lives by 1
+
+        if (lives > 0) // if you die, reload level with current world and stage you are on
+        {
+            LoadLevel(world, stage);
+        }
+        else // if out of lives then game over
+        {
+            GameOver(); 
+        }
+    }
+
+    private void GameOver()
+    {
+        NewGame(); // restarts the game
+    }
 }
