4e3552e366dbd043f3ad03cf063b2f5543f65e81
57qy654 <Jude.Pitschka@gmail.com>
2023-03-02 17:30:59 -0600

    Inititial Refresh

---
+++ Assets/Scripts/PlayerSpriteRenderer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpriteRenderer : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private PlayerMovement movement;
+
+    public Sprite idle;
+    public Sprite jump;
+    public Sprite slide;
+    public Sprite run;
+
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        movement = GetComponentInParent<PlayerMovement>();
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+}
