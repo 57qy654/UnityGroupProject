f5a5c193fc7252be4b9759259395edbf90a12467
ImSoBogus <willybogus11@gmail.com>
2023-03-13 13:06:13 -0500

    Goomba Script V1.0
    - added method that checks collision for player, if player lands on head it flattens goomba
    
    - added flatten method that flattens goomba and despawns it

--- Assets/Scripts/Goomba.cs
+++ Assets/Scripts/Goomba.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Goomba : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite flatSprite;
+
+    // checks collision of goomba and player
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))  // checks what goomba collides with
+        {
+            if (collision.transform.DotTest(transform, Vector2.down)) // checks if player lands on goomba head
+            {
+                Flatten();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // flatten method / death of goomba
+    private void Flatten()
     {
-        
+        GetComponent<Collider2D>().enabled = false; // disables goomba collider
+        GetComponent<EntityMovement>().enabled = false; // disables goomba movement
+        GetComponent<AnimatedSprite>().enabled = false; // disables goomba animations
+        GetComponent<SpriteRenderer>().sprite = flatSprite; // updates sprite to flat goomba
+        Destroy(gameObject, 0.5f); // destroys goomba after half a second, so you are able to see flat goomba
     }
 }
