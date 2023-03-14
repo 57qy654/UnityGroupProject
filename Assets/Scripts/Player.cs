543b808dfd2c6fbfd26142552e681892511b25a5
ImSoBogus <willybogus11@gmail.com>
2023-03-14 18:53:14 -0500

    Update 1 Player.cs
    - Added references
    - Added logic for giant and mini mario
    - Added Logic for mario death
    - Added functions that use the logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // references to the big mario and small mario sprite renderer 
+    public PlayerSpriteRenderer smallRenderer;
+    public PlayerSpriteRenderer bigRenderer;
+
+    // reference to the DeathAnimation script
+    private DeathAnimation deathAnimation;
+
+    // determines if mario is in big version or not
+    public bool big => bigRenderer.enabled;
+    public bool small => smallRenderer.enabled;
+
+    // determines if mario is in death animation or not
+    public bool dead => deathAnimation.enabled;
+
+
+    private void Awake()
     {
-        
+        deathAnimation = GetComponent<DeathAnimation>();
+    }
+    // function that specifies that mario was hit by something
+    public void Hit()
+    {
+        if (big)
+        {
+            Shrink();
+        }
+        else
+        {
+            Death();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // function that shrinks mario 
+    private void Shrink()
     {
-        
+        // TODO
     }
+
+    // function that kills mario
+    private void Death()
+    {
+        // TODO
+    }
+
 }
