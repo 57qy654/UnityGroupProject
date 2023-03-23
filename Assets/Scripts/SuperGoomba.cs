fa7969e7b3c199b416e8ed07ad39618818002473
ImSoBogus <willybogus11@gmail.com>
2023-03-22 21:17:52 -0500

    Adding Scripts and Adding Method to Super Goomba Script
    -added all the scripts
    -added method to supergoomba script that makes supergoomba powerful by having to have mario be in big form and stomp on him in order to kill him

--- Assets/Scripts/SuperGoomba.cs
+++ Assets/Scripts/SuperGoomba.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class SuperGoomba : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite flatSprite;
+
+    // checks collision of goomba and player
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))  // checks what goomba collides with
+        {
+            Player player = collision.gameObject.GetComponent<Player>(); // create reference to player script so you can call the player to get hit
+
+            if (player.starpower) // checks if the player is in starpower, if so, hits goomba
+            {
+                Hit();
+            }
+            else if (collision.transform.DotTest(transform, Vector2.down) && player.big) // checks if player is big and lands on goomba head
+            {
+                Flatten();
+            }
+            else
+            {
+                player.Hit();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other) //checks to see what trigger goomba enters into
     {
-        
+        if (other.gameObject.layer == LayerMask.NameToLayer("Shell")) // checks to see if goomba enters the shell trigger zone
+        {
+            Hit();
+        }
+
+    }
+
+    // flatten method / death of goomba
+    private void Flatten()
+    {
+        GetComponent<Collider2D>().enabled = false; // disables goomba collider
+        GetComponent<EntityMovement>().enabled = false; // disables goomba movement
+        GetComponent<AnimatedSprite>().enabled = false; // disables goomba animations
+        GetComponent<SpriteRenderer>().sprite = flatSprite; // updates sprite to flat goomba
+        Destroy(gameObject, 0.5f); // destroys goomba after half a second, so you are able to see flat goomba
+    }
+
+    private void Hit()
+    {
+        GetComponent<AnimatedSprite>().enabled = false; // reference to animated sprite script, stops animations
+        GetComponent<DeathAnimation>().enabled = true; // reference to death animation script, kills goomba and does the death animation
+        Destroy(gameObject, 3f); // destroy dead goomba after 3 seconds
+
     }
 }
