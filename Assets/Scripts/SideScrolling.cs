7e3ad1f8d5419f11bd54cfbb71489b0f8fec4820
57qy654 <Jude.Pitschka@gmail.com>
2023-04-20 00:27:09 -0500

    Flagpole Prefab
    and commented sidescrolling code

--- Assets/Scripts/SideScrolling.cs
+++ Assets/Scripts/SideScrolling.cs
@@ -20,21 +20,21 @@ public class SideScrolling : MonoBehaviour
 
     private void LateUpdate()
     {
-        Vector3 cameraPosition = transform.position;
-        cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x);
+        Vector3 cameraPosition = transform.position; // new vector3 to tranform camera
+        cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x); // sets the x value of the camera to the greater of the player's x or the cameras x
 
-        if (player.position.y >= camHeightAbove + 2f)
+        if (player.position.y >= camHeightAbove + 2f) // checks if player is 2 units above top camera position
         {
-            Vector3 targetPos = new Vector3(cameraPosition.x, camHeightAbove + 3f, cameraPosition.z);
-            cameraPosition = Vector3.Lerp(cameraPosition, targetPos, Time.deltaTime * 2f);
+            Vector3 targetPos = new Vector3(cameraPosition.x, camHeightAbove + 3f, cameraPosition.z); // new vector3 that takes in desired camera xy & z coordinates
+            cameraPosition = Vector3.Lerp(cameraPosition, targetPos, Time.deltaTime * 2f); // sets camera position to Vector3's Lerp function smoothly move it from current to new position
         }
-        else if (player.position.y + 2f < camHeightAbove)
+        else if (player.position.y + 2f < camHeightAbove) // checks if player's postion + 2 is less than that top camera position (+2 so it doesn't move too much)
         {
-            Vector3 targetPos = new Vector3(cameraPosition.x, camHeightNormal, cameraPosition.z);
-            cameraPosition = Vector3.Lerp(cameraPosition, targetPos, Time.deltaTime * 2f);
+            Vector3 targetPos = new Vector3(cameraPosition.x, camHeightNormal, cameraPosition.z); // new vector3 that takes in desired camera xy & z coordinates
+            cameraPosition = Vector3.Lerp(cameraPosition, targetPos, Time.deltaTime * 2f); // sets camera position to Vector3's Lerp function smoothly move it from current to new position
         }
 
-        transform.position = cameraPosition;
+        transform.position = cameraPosition; // transforms camera frame by frame
     }
 
     public void SetUnderGround(bool underground)
